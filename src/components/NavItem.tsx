26456934b428a6470d2e223cb5f39da7b93e53bc
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-14 11:54:01 -0600

    added component for individual items in navbar with logic

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client";
 
-const NavItem = () => {
-  return (
-    <div>NavItem</div>
-  )
+import React from "react";
+import { Button } from "./ui/button";
+import { PRODUCT_CATEGORIES } from "@/config";
+import { ChevronDown } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+// Category type from global constant
+type Category = (typeof PRODUCT_CATEGORIES)[number];
+
+// Prop properties
+interface NavItemProps {
+  category: Category;
+  handleOpen: () => void;
+  isOpen: boolean;
+  isAnyOpen: boolean;
 }
 
-export default NavItem
+// Component for individual items in navbar
+const NavItem = ({ isAnyOpen, category, handleOpen, isOpen }: NavItemProps) => {
+  return (
+    <div className="flex">
+      <div className="relative flex items-center">
+        <Button
+          className="gap-1.5"
+          onClick={handleOpen}
+          variant={isOpen ? "secondary" : "ghost"}
+        >
+          {category.label}
+          <ChevronDown
+            className={cn("h-4 w-4 transition-all text-muted-foreground", {
+              "-rotate-180": isOpen,
+            })}
+          />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NavItem;
