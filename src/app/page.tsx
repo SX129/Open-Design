9f3b50222e8966e4fb9349849fccce7fd253fdb4
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-14 10:41:32 -0600

    added perks constant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { Button, buttonVariants } from "@/components/ui/button";
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
 import Link from "next/link";
+
+const perks = [
+  {
+    name: "Instant Delivery",
+    Icon: ArrowDownToLine,
+    description: "Get your assets delivered to your email in seconds and download them right away.",
+  },
+  {
+    name: "Guaranteed Quality",
+    Icon: CheckCircle,
+    description: "Every asset on our platform is verified by our team to ensure our highest quality standard. Not happy? We offer a 30-day refund guarantee.",
+  },
+  {
+    name: "For the Planet",
+    Icon: Leaf,
+    description: "We've pledged 1% of sales to the preservation and restoration of the natural environment.",
+  },
+]
 
 export default function Home() {
   return (
+    <>
     <MaxWidthWrapper>
       <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
@@ -21,5 +41,14 @@ export default function Home() {
       </div>
       {/* TODO: List products */}
     </MaxWidthWrapper>
+
+    <section className="border-t border-gray-200 bg-gray-50">
+      <MaxWidthWrapper className="py-20">
+        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+
+        </div>
+      </MaxWidthWrapper>
+    </section>
+    </>
   );
 }
