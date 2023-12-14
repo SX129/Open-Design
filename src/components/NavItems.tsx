9dde251047ce0ee407107f7e57976b1aafc8b881
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-14 11:55:58 -0600

    imported navitem component

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,34 +1,43 @@
-"use client"
-import { PRODUCT_CATEGORIES } from '@/config'
-import React, { useState } from 'react'
+"use client";
+import { PRODUCT_CATEGORIES } from "@/config";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
 
 // Component for different items used on the navbar
 const NavItems = () => {
+  // State hook with <> typescript generic values of only null or number with a default value of null
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
 
-    // State hook with <> typescript generic values of only null or number with a default value of null
-    const [activeIndex, setActiveIndex] = useState<null | number>(null)
+  const isAnyOpen = activeIndex !== null
 
   return (
-    <div className='flex gap-4 h-full'>
+    <div className="flex gap-4 h-full">
       {PRODUCT_CATEGORIES.map((category, i) => {
-        
-        {/*Check for opened navbar items*/}
-        const handleOpen = () => {
-          if(activeIndex === i){
-            setActiveIndex(null)
-          } else{
-            setActiveIndex(i)
-          }
+        {
+          /*Check for opened navbar items*/
         }
+        const handleOpen = () => {
+          if (activeIndex === i) {
+            setActiveIndex(null);
+          } else {
+            setActiveIndex(i);
+          }
+        };
 
-        const isOpen = i === activeIndex
+        const isOpen = i === activeIndex;
 
         return (
-          
-        )
+          <NavItem
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            key={category.value}
+            isAnyOpen={isAnyOpen}
+          />
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default NavItems
+export default NavItems;
