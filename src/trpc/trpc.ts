35f60db37a524b9369553a7808d28d6e61d67898
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-18 22:40:11 -0600

    initalized trpc client with router and defined public api endpoint

---
+++ src/trpc/trpc.ts
@@ -0,0 +1,8 @@
+import { initTRPC } from "@trpc/server";
+
+// Initalizing trpc client with router
+const t = initTRPC.context().create();
+export const router = t.router;
+
+// Defining public API endpoint
+export const publicProcedure = t.procedure
