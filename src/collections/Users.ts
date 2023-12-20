0c55129ce1abe0e70314cad85edf9f96a06a6431
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-20 00:32:57 -0600

    defined collection types of users for auth

---
+++ src/collections/Users.ts
@@ -0,0 +1,27 @@
+import { CollectionConfig } from "payload/types";
+
+// Defining and authenticating types of users
+export const Users: CollectionConfig = {
+    slug: "users",
+    auth: true,
+    access: {
+        read: () => true,
+        create: () => true,
+    },
+    fields: [
+        {
+            name: "role",
+            type: "select",
+            options: [
+                {
+                    label: "Admin",
+                    value: "admin"
+                },
+                {
+                    label: "User",
+                    value: "user",
+                }
+            ]
+        },
+    ],
+}
