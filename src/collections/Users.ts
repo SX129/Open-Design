d3be744975b378dd605de694a56c8d8ddc6efece
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-20 00:37:47 -0600

    added required field

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -11,6 +11,7 @@ export const Users: CollectionConfig = {
     fields: [
         {
             name: "role",
+            required: true,
             admin: {
                 condition: () => false,
             },
