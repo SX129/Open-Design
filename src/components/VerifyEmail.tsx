6028f1b54bb5dadc435c09b963b243fddeb2fcf2
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-21 00:55:16 -0600

    added loading and success state

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -1,9 +1,11 @@
 "use client"
 
 import { trpc } from '@/trpc/client'
-import { XCircle } from 'lucide-react'
+import { Loader2, XCircle } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { buttonVariants } from './ui/button'
 
 interface VerifyEmailProps {
     token: string
@@ -34,6 +36,21 @@ const VerifyEmail = ({token}: VerifyEmailProps) => {
                 <div className='relative mb-4 h-60 w-60 text-muted-foreground'>
                     <Image src='/hippo-email-sent.png' fill alt='email was sent' />
                 </div>
+                <h3 className='font-semibold text-2xl'>You&apos;re all set!</h3>
+                <p className='text-muted-foreground text-center mt-1'>Thank you for verifying your email.</p>
+                <Link className={buttonVariants({className: 'mt-4'})} href='/sign-in'>Sign In</Link>
+            </div>
+        )
+    }
+
+    if(isLoading){
+        return (
+            <div className='flex flex-col items-center gap-2'>
+                <Loader2 className='animate-spin h-8 w-8 text-zinc-300' />
+                <h3 className='font-semibold text-xl'>Verifying...</h3>
+                <p className='text-muted-foreground text-sm'>
+                    This won&apos;t take long.
+                </p>
             </div>
         )
     }
