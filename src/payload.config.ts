ad47b92910ec4e23e5729803940037d59d7838eb
Henry Nguyen <nguyenhenry682@gmail.com>
2023-12-25 21:34:39 -0600

    added media collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 import { Users } from "./collections/Users";
 import dotenv from "dotenv";
 import { Products } from "./collections/Products/Products";
+import { Media } from "./collections/Media";
 
 dotenv.config({
     path: path.resolve(__dirname, "../.env")
@@ -14,7 +15,7 @@ dotenv.config({
 // Properties for CMS console
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-    collections: [Users, Products],
+    collections: [Users, Products, Media],
     routes: {
         admin: '/sell'
     },
