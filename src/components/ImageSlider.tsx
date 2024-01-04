634700d732d6cd9bf0ecc12ead67316e291ef7ee
Henry Nguyen <nguyenhenry682@gmail.com>
2024-01-03 22:42:36 -0600

    added load param for effect hook

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -28,7 +28,7 @@ const ImageSlider = ({urls}: ImageSliderProps) => {
                 isEnd: activeIndex === (urls.length ?? 0) - 1
             })
         })
-    })
+    }, [swiper, urls])
 
     const activeStyles = "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300"
 
