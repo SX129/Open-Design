03b09aef3751772454bc56ee7dc8e06b53476936
Henry Nguyen <nguyenhenry682@gmail.com>
2024-01-09 20:05:03 -0600

    added cart hook

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,24 +1,35 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from 'react'
-import { Button } from './ui/button'
+import React, { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { useCart } from "@/hooks/use-cart";
+import { Product } from "@/payload-types";
 
 // Component for cart button on product page
-const AddToCartButton = () => {
+const AddToCartButton = ({product}: {product: Product}) => {
+  const { addItem } = useCart();
+  const [isSuccess, setIsSuccess] = useState<boolean>(false);
 
-    const [isSuccess, setIsSuccess] = useState<boolean>(false)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false);
+    }, 2000);
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setIsSuccess(false)
-        }, 2000)
-
-        return () => clearTimeout(timeout)
-    }, [])
+    return () => clearTimeout(timeout);
+  }, []);
 
   return (
-    <Button onClick={() => {setIsSuccess(true)}} size='lg' className='w-full'>{isSuccess ? "Added!" : "Add to cart"}</Button>
-  )
-}
+    <Button
+      onClick={() => {
+        addItem(product)
+        setIsSuccess(true)
+      }}
+      size="lg"
+      className="w-full"
+    >
+      {isSuccess ? "Added!" : "Add to cart"}
+    </Button>
+  );
+};
 
-export default AddToCartButton
+export default AddToCartButton;
