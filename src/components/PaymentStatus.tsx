3591021b9e2e9c46fd0acacdec7da73ca1e2740c
Henry Nguyen <nguyenhenry682@gmail.com>
2024-01-10 16:41:32 -0600

    added component to display client side order status

---
+++ src/components/PaymentStatus.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React from 'react'
+
+interface PaymentStatusProps {
+    orderEmail: string,
+    orderId: string,
+    isPaid: boolean
+}
+
+// Component for client side user order status
+const PaymentStatus = ({orderEmail, orderId, isPaid}: PaymentStatusProps) => {
+
+  return (
+    <div className='mt-16 grid grid-cols-2 gap-x-4 text-sm text-gray-600'>
+        <div>
+            <p className='font-medium text-gray-900'>
+                Shipping To
+            </p>
+            <p>{orderEmail}</p>
+        </div>
+
+        <div>
+            <p className='font-medium text-gray-900'>
+                Order Status
+            </p>
+            <p>
+                {isPaid ? "Payment successful" : "Pending payment"}
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default PaymentStatus
