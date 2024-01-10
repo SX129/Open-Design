b156bef25e69d6c2d46ef31fb5b3b567fbea74af
Henry Nguyen <nguyenhenry682@gmail.com>
2024-01-10 00:03:24 -0600

    added hooks to maintain mount state

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,12 +3,18 @@
 import { useCart } from '@/hooks/use-cart'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 //Landing page for checkout cart
 const page = () => {
 
     const {items, removeItem} = useCart()
+
+    const [isMounted, setIsMounted] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
 
   return (
     <div className='bg-white'>
@@ -19,13 +25,13 @@ const page = () => {
 
             <div className='mt-12 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12 xl:gap-x-16'>
                 <div className={cn("lg:col-span-7", {
-                    "rounded-lg border-2 border-dashed border-zinc-200 p-12": items.length === 0,
+                    "rounded-lg border-2 border-dashed border-zinc-200 p-12": isMounted && items.length === 0,
                 })}>
                     <h2 className='sr-only'>
                         Items in your shopping cart
                     </h2>
 
-                    {items.length === 0 ? (
+                    {isMounted && items.length === 0 ? (
                         <div className='flex h-full flex-col items-center justify-center space-y-1'>
                             <div aria-hidden="true" className='relative mb-4 h-40 w-40 text-muted-foreground'>
                                 <Image src="/hippo-empty-cart.png" fill loading='eager' alt='empty shopping cart hippo'/>
