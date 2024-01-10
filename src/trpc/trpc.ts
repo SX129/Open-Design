c0a93f2735a25f2c571de60ebbef114f087f5a11
Henry Nguyen <nguyenhenry682@gmail.com>
2024-01-10 01:52:26 -0600

    added private endpoint

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,9 +1,31 @@
+import { User } from "@/payload-types";
 import { ExpressContext } from "@/server";
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
+import { PayloadRequest } from "payload/types";
 
 // Initalizing trpc client with router
 const t = initTRPC.context<ExpressContext>().create();
 export const router = t.router;
 
+const middlware = t.middleware
+const isAuth = middlware(async ({ctx, next}) => {
+    const req = ctx.req as PayloadRequest
+
+    const {user} = req as {user: User | null}
+
+    if (!user || !user.id){
+        throw new TRPCError({ code: "UNAUTHORIZED"})
+    }
+
+    return next({
+        ctx: {
+            user,
+        },
+    })
+})
+
 // Defining public API endpoint
 export const publicProcedure = t.procedure
+
+// Defining private API endpoint
+export const privateProcedure = t.procedure.use(isAuth)
